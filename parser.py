52ec04c301271bcc3c13902ff5ed09f72c87baf2
hannahsim <ssim@g.harvard.edu>
2017-10-15 16:54:23 -0400

    Added parser object.

---
+++ parser.py
@@ -0,0 +1,14 @@
+
+"""Module with parser for xml file"""
+
+import numpy as np
+import xml.etree.ElementTree as ET
+
+class ReactionParser():
+
+    def __init__(self, xml_filename):
+        self.xml_filename = xml_filename
+
+    def __call__(self):
+        pass
+
