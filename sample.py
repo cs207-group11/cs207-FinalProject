49ebf8b896dbed2cc2c700dcda8908dd4294afa2
hannahsim <ssim@g.harvard.edu>
2017-10-15 16:16:25 -0400

    Removed placeholder sample files.

--- sample.py
+++
@@ -1,7 +0,0 @@
-
-"""Sample module."""
-
-import numpy as np 
-
-def add_nums(x, y):
-    return x + y
--- test_sample.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-from sample import *
-
-def test_add():
-    x = 2
-    y = 5
-    test_sum = add_nums(x, y)
-    assert test_sum == 7
