2c6455669572150b718a4e77442ac24776bb520d
haixing-yin <yinhaixing007@gmail.com>
2017-11-06 22:06:00 -0500

    Revert "edit class parser"

--- chemkin.py
+++ chemkin.py
@@ -1,3 +1,4 @@
+
 """Module for classes and functions in ChemKinLib."""
 
 import numbers
@@ -5,6 +6,7 @@ import numpy
 import warnings
 import os.path
 import xml.etree.ElementTree as ET
+
 
 class ReactionParser():
     """Class for parsing input xml file describing reactions"""
@@ -21,8 +23,6 @@ class ReactionParser():
         """
         if os.path.isfile(xml_filename):
             self.xml_filename = xml_filename
-            tree = ET.parse(self.xml_filename)
-            self.rxns = tree.getroot()
         else:
             raise IOError("Reaction (xml) file not found!")
                       
@@ -36,203 +36,109 @@ class ReactionParser():
         self.reaction_list = []
 
     def __call__(self):
-        """Parser all information for all reactions by calling self.append_rxn function
+        """Parser all information for all reactions
         INPUTS
         ======
         self : contains xml file name: self.xml_filename 
                 which is our target file to parser
         """
-        self.append_rxn()
-    
-    def get_species(self):
-        """get reaction species
-        INPUTS:
-        -------
-        self : we need self for xml parsed content
-        
-        RETURNS
-        ========
-        species: a list of reaction species as dictionary
-                as the form key = species name, value = None,
-                value will be filled after we get the concentration from user.
-        """
+        tree = ET.parse(self.xml_filename)
+        rxns = tree.getroot()
+
         # species
-        phase = self.rxns.findall('phase')
+        phase = rxns.findall('phase')
         species_list = []
-        for phase in self.rxns.findall('phase'):
+        for phase in rxns.findall('phase'):
             species = phase.find('speciesArray').text
         species_list = species.split()
         self.species = {}
         for specie in species_list:
             self.species[specie] = None
-        return self.species
         
-    def get_rxn_type(self,reaction):
-        """get reaction type
-        INPUTS:
-        -------
-        reaction: parsed xml file which contains information about reactions
-        
-        RETURNS
-        ========
-        rxn_type: a string describe reaction's type, such as "elementary".
-        """
-        rxn_type = reaction.get('type')
-        return rxn_type
-        
-    def get_is_reversible(self,reaction):
-        """get information about whether the reaction is reversible
-        INPUTS:
-        -------
-        reaction: parsed xml file which contains information about reactions
-        
-        RETURNS
-        ========
-        is_reversible: a boolean, True = reversible and False = irreversible
-        """
-        # get is_reversible
-        if reaction.get('reversible') == "yes":
-            is_reversible = True
-        else:
-            is_reversible = False
-        return is_reversible
-    
-    def get_rxn_equation(self,reaction):
-        """get reaction equation
-        INPUTS:
-        -------
-        reaction: parsed xml file which contains information about reactions
-        
-        RETURNS
-        ========
-        rxn_equation: a string, reaction equation
-        """
-        # rxn_equation
-        rxn_equation = reaction.find('equation').text
-        return rxn_equation
-    
-    def get_rate_coeffs_components(self,reaction):
-        """get rate coeffs components based on type of coefficient type
-        INPUTS:
-        -------
-        reaction: parsed xml file which contains information about reactions
-        
-        RETURNS
-        ========
-        rate_coeffs_components: a dictionary, as the form of {coefficient name: coefficient value}. 
-        """
-        # reaction_coef
-        for coef in reaction.findall('rateCoeff'):
-            # Arrhenius
-            if coef.find('Arrhenius') is not None:
-                for arr in coef.findall('Arrhenius'):
-                    if arr.find('A') is None or arr.find('A').text is None:
-                        raise ValueError("Didn't provide coefficient A for modified Arrhenius ")
-                    else:
-                        A = float(arr.find('A').text)
-                    if arr.find('E') is None or arr.find('E').text is None:
-                        raise ValueError("Didn't provide coefficient E for modified Arrhenius ")
-                    else:
-                        E = float(arr.find('E').text)
-                rate_coeffs_components = {
-                    "A":A,
-                    "E":E
-                }
-
-            # modified Arrhenius
-            if coef.find('modifiedArrhenius') is not None:
-                for arr in coef.findall('modifiedArrhenius'):
-                    if arr.find('A') is None or arr.find('A').text is None:
-                        raise ValueError("Didn't provide coefficient A for modified Arrhenius ")
-                    else:
-                        A = float(arr.find('A').text)
-                    if arr.find('b') is None or arr.find('b').text is None:
-                        raise ValueError("Didn't provide coefficient b for modified Arrhenius ")
-                    else:
-                        b = float(arr.find('b').text)
-                    if arr.find('E') is None or arr.find('E').text is None:
-                        raise ValueError("Didn't provide coefficient E for modified Arrhenius ")
-                    else:
-                        E = float(arr.find('E').text)
-                rate_coeffs_components = {
-                    "A":A,
-                    "b":b,
-                    "E":E
-                }
-
-            # constant
-            if coef.find('Constant') is not None:
-                for arr in coef.findall('Constant'):
-                    if arr.find('k') is None or arr.find('k').text is None:
-                        raise ValueError("Didn't provide coefficient k for modified Arrhenius ")
-                    else:
-                        k = float(arr.find('k').text)
-                rate_coeffs_components = {
-                    "k":k
-                }
-            return rate_coeffs_components
-
-    def get_reactant_stoich_coeffs(self,reaction):
-        """get reactant stoichiometric coefficients
-        INPUTS:
-        -------
-        reaction: parsed xml file which contains information about reactions
-        
-        RETURNS
-        ========
-        reactant_stoich_coeffs: a dictionary, as the form of {reactant name: coefficient}. 
-        """
-        # reactant_stoich_coeffs
-        reactant_stoich_coeffs = {}
-        for reactant in reaction.find('reactants').text.split():
-            key = reactant.split(":")[0]
-            value = reactant.split(":")[1]
-            reactant_stoich_coeffs[key] = int(value)
-        return reactant_stoich_coeffs
-    
-    def get_product_stoich_coeffs(self,reaction):
-        """get product stoichiometric coefficients
-        INPUTS:
-        -------
-        reaction: parsed xml file which contains information about reactions
-        
-        RETURNS
-        ========
-        product_stoich_coeffs: a dictionary, as the form of {product name: coefficient}. 
-        """
-        # product_stoich_coeffs
-        product_stoich_coeffs = {}
-        for product in reaction.find('products').text.split():
-            key = product.split(":")[0]
-            value = product.split(":")[1]
-            product_stoich_coeffs[key] = int(value)
-        return product_stoich_coeffs
-    
-    def append_rxn(self):
-        """loop through the parsed xml file, then loop through each reaction and get their information
-            such as reaction type, create a Reaction object for each reaction. Append each Reaction 
-            object to self.reaction_list
-        INPUTS:
-        -------
-        self: parsed xml file
-        """
-        for reactionData in self.rxns.findall('reactionData'):
+        # rxns
+        for reactionData in rxns.findall('reactionData'):
             for reaction in reactionData.findall('reaction'):
-                # get species
-                self.get_species()
-                # get is_reversible
-                is_reversible = self.get_is_reversible(reaction) 
-                # type
-                rxn_type = self.get_rxn_type(reaction)
-                # rxn_equation
-                rxn_equation = self.get_rxn_equation(reaction)
-                # reaction_coef
-                rate_coeffs_components = self.get_rate_coeffs_components(reaction)
-                # reactant_stoich_coeffs
-                reactant_stoich_coeffs = self.get_reactant_stoich_coeffs(reaction)
-                # product_stoich_coeffs
-                product_stoich_coeffs = self.get_product_stoich_coeffs(reaction)
                 
+                # get id
+                Id = reaction.get('id')
+                
+                # get is_reversible
+                if reaction.get('reversible') == "yes":
+                    is_reversible = True
+                else:
+                    is_reversible = False
+                
+                # type
+                rxn_type = reaction.get('type')
+
+                # rxn_equation
+                rxn_equation = reaction.find('equation').text
+
+                # reaction_coef
+                for coef in reaction.findall('rateCoeff'):
+
+                    # Arrhenius
+                    if coef.find('Arrhenius') is not None:
+                        for arr in coef.findall('Arrhenius'):
+                            if arr.find('A') is None or arr.find('A').text is None:
+                                raise ValueError("Didn't provide coefficient A for modified Arrhenius ")
+                            else:
+                                A = float(arr.find('A').text)
+                            if arr.find('E') is None or arr.find('E').text is None:
+                                raise ValueError("Didn't provide coefficient E for modified Arrhenius ")
+                            else:
+                                E = float(arr.find('E').text)
+                        rate_coeffs_components = {
+                            "A":A,
+                            "E":E
+                        }
+
+                    # modified Arrhenius
+                    if coef.find('modifiedArrhenius') is not None:
+                        for arr in coef.findall('modifiedArrhenius'):
+                            if arr.find('A') is None or arr.find('A').text is None:
+                                raise ValueError("Didn't provide coefficient A for modified Arrhenius ")
+                            else:
+                                A = float(arr.find('A').text)
+                            if arr.find('b') is None or arr.find('b').text is None:
+                                raise ValueError("Didn't provide coefficient b for modified Arrhenius ")
+                            else:
+                                b = float(arr.find('b').text)
+                            if arr.find('E') is None or arr.find('E').text is None:
+                                raise ValueError("Didn't provide coefficient E for modified Arrhenius ")
+                            else:
+                                E = float(arr.find('E').text)
+                        rate_coeffs_components = {
+                            "A":A,
+                            "b":b,
+                            "E":E
+                        }
+
+                    # constant
+                    if coef.find('Constant') is not None:
+                        for arr in coef.findall('Constant'):
+                            if arr.find('k') is None or arr.find('k').text is None:
+                                raise ValueError("Didn't provide coefficient k for modified Arrhenius ")
+                            else:
+                                k = float(arr.find('k').text)
+                        rate_coeffs_components = {
+                            "k":k
+                        }
+
+                # reactant_stoich_coeffs
+                reactant_stoich_coeffs = {}
+                for reactant in reaction.find('reactants').text.split():
+                    key = reactant.split(":")[0]
+                    value = reactant.split(":")[1]
+                    reactant_stoich_coeffs[key] = int(value)
+
+                # product_stoich_coeffs
+                product_stoich_coeffs = {}
+                for product in reaction.find('products').text.split():
+                    key = product.split(":")[0]
+                    value = product.split(":")[1]
+                    product_stoich_coeffs[key] = int(value)
+
                 if is_reversible == False and rxn_type == "Elementary":
                     rxn = Reaction(rxn_type, is_reversible, rxn_equation, 
                                            self.species, rate_coeffs_components,
@@ -241,6 +147,7 @@ class ReactionParser():
                 
                 else:
                     raise NotImplementedError("This type of reaction has not been implemented yet!")
+
 
 class Reaction():
     """Base class for an elementary (irreversible) reaction"""
--- test_chemkin.py
+++ test_chemkin.py
@@ -313,57 +313,47 @@ def test_ReactionCoeff_mod_arrhenius_changing_R():
         k_test = ReactionCoeff(k_parameters, T).k
 
 def test_ReactionParser_IOError():
-    """Test when wrong xml is passed to parser"""
     try:
         parser = ReactionParser("no_such_file")
     except IOError as err:
         assert(type(err) == IOError)
 
 def test_ReactionParser_species():
-    """Test when reaction rate coefficient is modified
-    Arrhenius but R is changed by user"""
     xml_filename = "rxns.xml"
     parser = ReactionParser(xml_filename)
     parser()
     assert parser.species == {'H':None, 'O':None, 'OH':None, 'H2':None, 'H2O':None, 'O2':None}
     
 def test_ReactionParser_type():
-    """Test get_rxn_type() for an elementary reaction."""
     xml_filename = "rxns.xml"
     parser = ReactionParser(xml_filename)
     parser()
     assert parser.reaction_list[0].rxn_type == 'Elementary'
     
 def test_ReactionParser_rate_coeffs_components():
-    """Test get_rate_coeffs_components for reaction 1."""
     xml_filename = "rxns.xml"
     parser = ReactionParser(xml_filename)
     parser()
     assert parser.reaction_list[0].rate_coeffs_components == {'A': 35200000000.0, 'E': 71400.0}
-    
 def test_ReactionParser_is_reversible():
-    """Test get_is_reversible for reaction irreversible reaction."""
     xml_filename = "rxns.xml"
     parser = ReactionParser(xml_filename)
     parser()
     assert parser.reaction_list[0].is_reversible == False
 
 def test_ReactionParser_rxn_equation():
-    """Test get_rxn_equation for reaction 1."""
     xml_filename = "rxns.xml"
     parser = ReactionParser(xml_filename)
     parser()
     assert parser.reaction_list[0].rxn_equation == 'H + O2 =] OH + O'
     
 def test_ReactionParser_reactant_stoich_coeffs():
-    """Test get_reactant_stoich_coeffs for reaction 1."""
     xml_filename = "rxns.xml"
     parser = ReactionParser(xml_filename)
     parser()
     assert parser.reaction_list[0].reactant_stoich_coeffs == {'H': 1, 'H2': 0, 'H2O': 0, 'O': 0, 'O2': 1, 'OH': 0}
 
 def test_ReactionParser_product_stoich_coeffs():
-    """Test get_product_stoich_coeffs for reaction 1."""
     xml_filename = "rxns.xml"
     parser = ReactionParser(xml_filename)
     parser()
@@ -437,3 +427,7 @@ def test_overall_workflow_elementary_rxn():
     rxnrate = rxn1.compute_reaction_rate()
     expected = 0.0
     assert rxnrate == expected   
+    
+
+
+
