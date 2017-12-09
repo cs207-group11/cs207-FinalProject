378627984c54cceb6b1249b6fb4686a534da426d
Hannah Sim <hsim13372@gmail.com>
2017-12-09 16:50:23 -0500

    Update visualizer.py

--- src/chemkinlib/utils/visualizer.py
+++ src/chemkinlib/utils/visualizer.py
@@ -1,11 +1,12 @@
 
 from graphviz import Digraph
 import random
-from moviepy.editor import *
 import numpy as np
 import os.path
 from chemkinlib.reactions import ReactionSystems
 import imageio
+imageio.plugins.ffmpeg.download()
+from moviepy.editor import *
 
 
 class ReactionPathDiagram():
